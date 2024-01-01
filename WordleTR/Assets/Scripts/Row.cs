481e29841616d0fc797f93bf0c2fac4c74966e8f
Rustu.Inan <rustu.inan00@gmail.com>
2024-01-01 05:41:55 +0300

    Project Completed

--- WordleTR/Assets/Scripts/Row.cs
+++ WordleTR/Assets/Scripts/Row.cs
@@ -1,8 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Row : MonoBehaviour
 {
-    
+    public Tile[] tiles { get; private set; }
+
+    public string Word
+    {
+        get
+        {
+            string word = "";
+
+            for (int i = 0; i < tiles.Length; i++) {
+                word += tiles[i].letter;
+            }
+
+            return word;
+        }
+    }
+
+    private void Awake()
+    {
+        tiles = GetComponentsInChildren<Tile>();
+    }
+
 }
